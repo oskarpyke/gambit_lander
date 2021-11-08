77fd3ab51e65e2e397b0d6f7a06c9309b3583e49
Oskar Pyke <oskar@pyke.dev>
2021-11-08 22:12:38 +0100

    init commit

---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="backgr">
+    <h1 class="title" style="color: white; margin: auto; margin-top: 25%">
+      gambit.ai
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.backgr {
+  display: grid;
+}
+
+.title {
+  font-family: "Brown";
+  font-size: 6em;
+}
+
+body,
+html {
+  background: black;
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  min-height: 100%;
+}
+</style>
